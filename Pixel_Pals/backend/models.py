a203633fba29ce319559cca1100a84e3dd60bef6
Tristan White <tristanwhite.gva@gmail.com>
2020-02-18 13:34:54 -0600

    Started modules, Admin site crashes python

--- Pixel_Pals/backend/models.py
+++ Pixel_Pals/backend/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
+
+'''
+    Model for user
+    UserID
+    Name
+    Email
+    Password
+    Wishlist
+'''
+
+
+'''
+    Graphics card
+    
+        Model #
+        Name
+'''
+class GraphicsCard(models.Model):
+
+    metaData = JSONField()
+
+
+
+'''
+    amazon price table
+    Price URL
+    Avail
+'''
+class AmaxonPriceTable(models.Model):
+
+    metaData = JSONField()
+
+
+'''
+    newegg price table
+'''
+
+
+'''
+    ideal data
+    
+    lowest price
+        Url
+        Available
+'''
