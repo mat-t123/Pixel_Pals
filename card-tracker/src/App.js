25af55c23df53346b6b138aeda58019b9962d024
Wilkrest <jarirez@hotmail.com>
2020-03-01 22:15:06 -0600

    added node modules and cleaned the folder

---
+++ card-tracker/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+//import { unstable_batchedUpdates } from 'react-dom';
+
+function App() {
+  return (
+    <>
+      <div> CARD TRACKER </div>
+      <SearchBar />
+      <button>Search</button>
+    </>
+  );
+}
+
+export default App;
---
+++ card-tracker/src/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SearchBar() {
+    return (
+        <div>
+            <input type="text" id="cardSearchBar" placeholder="Search for cards.."></input>
+        </div>
+    )
+}
