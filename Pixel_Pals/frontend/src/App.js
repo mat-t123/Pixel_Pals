d34241cd4a88d89e8454ad4da66f0d312e3e5830
Wilkrest <jarirez@hotmail.com>
2020-03-07 17:43:44 -0600

    installed router library for react

--- Pixel_Pals/frontend/src/App.js
+++ Pixel_Pals/frontend/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import SearchBar from './SearchBar'
-//import { unstable_batchedUpdates } from 'react-dom';
+import './App.css';
+import MainSearch from './MainSearch';
+import CardDisplay from './CardDisplay';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <div> CARD TRACKER </div>
-      <SearchBar />
-      <button>Search</button>
-    </>
-  );
-}
+    <Router>
+      <div className ="App">
+        <MainSearch />
+        <CardDisplay />
+      </div>
+    </Router>
+  ); 
+}       
 
 export default App;
---
+++ Pixel_Pals/frontend/src/CardDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CardDisplay() {
+    return (
+        <div>
+            <h1>Card Result Page</h1>
+        </div>
+    )
+}
