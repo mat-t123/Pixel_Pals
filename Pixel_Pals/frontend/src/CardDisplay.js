a461bdc7af0037e310eb92493b6fd02d1b405f4f
Wilkrest <jarirez@hotmail.com>
2020-03-11 17:10:33 -0500

    added card display functionality

--- Pixel_Pals/frontend/src/CardDisplay.js
+++ Pixel_Pals/frontend/src/CardDisplay.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css'
 
 export default function CardDisplay() {
+
+    const cards = [
+        {
+            card_id: "the first card",
+            name: "Titan GFX 20"
+        }
+    ]
+
+
+
     return (
-        <div>
-            <h1>Card Result Page</h1>
+        <div className="card-result-container">
+            <h3>Card Result Page</h3>
+            <div className="results-card-info">
+                <div>card image</div>
+                <div>{cards[0].name} </div>
+            </div>
+            <div className="card-graph-container">
+                <div>Card Graph from Amazon</div>
+                <div>Card Graph from NewEgg</div>
+            </div>
+            <button>Add to watchlist</button>
         </div>
     )
 }
--- Pixel_Pals/frontend/src/MainSearch.js
+++ Pixel_Pals/frontend/src/MainSearch.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import './App.css';
 
 export default function MainSearch() {
     return (
-        <body>
-            <div>
-                <input type="search" id="mainSearchBar" placeholder="Search for cards.."></input>
-                <Link to="/cardDisplay">
-                    <button type="button" id="mainSearchButton">Search</button>
-                </Link>
-            </div>
-        </body>
+        <div>
+            <input type="search" id="mainSearchBar" placeholder="Search for cards.."></input>
+            <Link to="/cardDisplay">
+                <button type="button" id="mainSearchButton">Search</button>
+            </Link>
+        </div>
     )
 }
